dbe63715196eb6ce603fe8d62b6307a8f3a83eb9
ezeutno <ivan.suratno@gmail.com>
2021-08-30 06:17:21 +0700

    Smooth Animation on About

--- js/animate.js
+++ js/animate.js
@@ -1,4 +1,7 @@
 function aboutButtonClick() {
+    var el0 = document.getElementById("fix-ann");
+    el0.classList.toggle('ann-none');
+
     var el1 = document.getElementById("ann-about");
     el1.classList.toggle('ann');
 };

68e9441f1966519226d54584c6720d204988ac17
ezeutno <ivan.suratno@gmail.com>
2021-08-30 06:32:16 +0700

    Add animation for All Button

--- js/animate.js
+++ js/animate.js
@@ -1,19 +1,8 @@
-function aboutButtonClick() {
-    var el0 = document.getElementById("fix-ann");
+function buttonClick(loc) {
+    console.log(loc)
+    var el0 = document.getElementById("fix-"+loc);
     el0.classList.toggle('ann-none');
 
-    var el1 = document.getElementById("ann-about");
-    el1.classList.toggle('ann');
-};
-
-
-function contactUsButtonClick() {
-    var el1 = document.getElementById("ann-about");
-    el1.classList.toggle('ann');
-};
-
-
-function eventsButtonClick() {
-    var el1 = document.getElementById("ann-about");
+    var el1 = document.getElementById("ann-"+loc);
     el1.classList.toggle('ann');
 };

fe3306e5bed17efcad812edd4b49d0d15267d988
ezeutno <ivan.suratno@gmail.com>
2021-08-30 05:32:04 +0700

    Add info page

--- js/animate.js
+++ js/animate.js
@@ -2,3 +2,15 @@ function aboutButtonClick() {
     var el1 = document.getElementById("ann-about");
     el1.classList.toggle('ann');
 };
+
+
+function contactUsButtonClick() {
+    var el1 = document.getElementById("ann-about");
+    el1.classList.toggle('ann');
+};
+
+
+function eventsButtonClick() {
+    var el1 = document.getElementById("ann-about");
+    el1.classList.toggle('ann');
+};

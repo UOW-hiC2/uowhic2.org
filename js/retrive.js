e299ad5fbe3ce9a276f4738a5f7601837fa211be
ezeutno <ivan.suratno@gmail.com>
2021-08-31 04:35:33 +0700

    Update display for Event Tab

--- js/retrive.js
+++ js/retrive.js
@@ -60,29 +60,36 @@ function about(data) {
 }
 
 function eventFunc(data) {
-    // let tab = `<h1>Events</h1>`;
-    // tab += `
-    //     <div class="fix-fil">
-    //     </div>
-    // `
-    // tab += `<div><h2>Coming Soon</h2></div>`
+    let tab = ``
     
     // Loop to access all rows 
-    // for (let social of data.socials) {
-    //     tab +=  
-    //     `
-    //     <div class="inner" style="cursor: pointer;" onclick="window.location='${social.link}';">
-    //         <div class="paddingIcon">
-    //             <image src="${social.icon}">
-    //         </div>
-    //         <span class="clearOnMobile">
-    //             <h3>${social.name}</h3>
-    //         </span>
-    //     </div>
-    //     `;
-    // }
+    for (let event of data.events) {
+        if (event.schedulled) {
+            let fileExtention = event.poster.split('.').pop();
+            if (["mp4"].includes(fileExtention)) {
+                tab += `
+                    <video autoplay loop class="event-poster" src="${event.poster}" type="video/mp4"></video>
+                `
+            } else {
+                tab += `
+                    <img class="event-poster" src="${event.poster}"/>
+                `
+            }
+
+            tab +=`
+            <div>
+                <h2>Q/A with Industry Professionals</h2>
+                <h3>Introduction to UoW hiC2 and a QnA with Google, Amazon, & Vimy regarding CyberSecurity, Cryptography and It's Job Opportunity</h3>
+                <button class="inner" onclick="window.location.href='https://uowhic2.org/event'">
+                    <h3>Zoom Link</h3>
+                </button>
+            </div>
+            `;
+        }
+    }
+
     // Setting innerHTML as tab variable
-    // document.getElementById("ann-events").innerHTML = tab;
+    document.getElementById("con-events").innerHTML = tab;
 }
 
 // Calling that async function
